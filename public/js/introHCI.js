25efd1dd329c65d347269d960f6085ae427cdc4c
Yun Tang <yut039@ucsd.edu>
2017-02-16 12:25:24 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,45 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/' + idNumber, addProject);
 }
 
+function addProject(result){
+	console.log(result['id']);
+
+	var projectHTML=
+	                '<img class="detailsImage" src="'+ result['image']+'" class="img">' +
+	                '<p><small>' +result['title']+'<small></p>'+
+	                '<p><small>' + result['date']+'</small></p>'+
+	                '<p>' +result['summary']+'</p>';
+
+	$("#project"+result['id']+" .details").html(projectHTML);
+
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', changeColor);
 }
+
+function changeColor(result){
+console.log(result);
+var color = result.color;
+//var color=result.hex;
+$('body').css('background-color', color[0]);
+$('.thumbnail').css('background-color', color[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', color[2]);
+$('p').css('color', color[3]);
+$('.project img').css('opacity', .75);
+}
+
+
+
+
+
+
+
